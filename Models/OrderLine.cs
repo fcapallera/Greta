8271bfe0282e93aeb976026ee5e6c27871137f22
fcapallera <fcapallera@gmail.com>
2019-11-18 09:35:13 +0100

    Created ORM Models with EF Core

---
+++ Models/OrderLine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreBot.Models
+{
+    public partial class OrderLine
+    {
+        public int Id { get; set; }
+        public int Product { get; set; }
+        public int Amount { get; set; }
+        public int UserId { get; set; }
+
+        public UserProfile User { get; set; }
+    }
+}
---
+++ Store/Entity/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CoreBot.Store.Entity
+{
+    [XmlRoot("prestashop")]
+    public class Product
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+
+        [XmlAttribute("id_manufacturer")]
+        public int ManufacturerId { get; set; }
+
+        [XmlAttribute("id_supplier")]
+        public int SupplierId { get; set; }
+    }
+}
