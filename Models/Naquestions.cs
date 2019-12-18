8ae52928573ebd1f8c9fc88908211909f2d60c73
fcapallera <fcapallera@gmail.com>
2019-12-18 10:18:19 +0100

    Added DBContext with EF ORM

---
+++ Models/Naquestions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreBot.Models
+{
+    public partial class Naquestions
+    {
+        public int QuestionId { get; set; }
+        public string QuestionText { get; set; }
+        public int UserId { get; set; }
+
+        public UserProfile User { get; set; }
+    }
+}
---
+++ Models/UserProfile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreBot.Models
+{
+    public partial class UserProfile
+    {
+        public UserProfile()
+        {
+            Naquestions = new HashSet<Naquestions>();
+            OrderLine = new HashSet<OrderLine>();
+        }
+
+        public UserProfile(int permission)
+        {
+            Permission = permission;
+            Naquestions = new HashSet<Naquestions>();
+            OrderLine = new HashSet<OrderLine>();
+        }
+
+        public int Id { get; set; }
+        public int? PrestashopId { get; set; }
+        public bool Welcomed { get; set; }
+        public int Permission { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public ICollection<Naquestions> Naquestions { get; set; }
+        public ICollection<OrderLine> OrderLine { get; set; }
+    }
+}
