9df657351fb1f323da68a466aa4a97c9059036e4
fcapallera <fcapallera@gmail.com>
2019-12-18 10:20:18 +0100

    Implemented extension for stepContext Value

---
+++ Extensions/StepContextExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBot.Extensions
+{
+    public static class StepContextExtensions
+    {
+        public static T GetValue<T>(this WaterfallStepContext stepContext, string index)
+        {
+            if (stepContext.Values.TryGetValue(index, out object value))
+                return (T)value;
+
+            else throw new KeyNotFoundException();
+        }
+    }
+}
