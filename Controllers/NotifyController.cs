3f97339d693a41fad888c2a60c481ee0e84e3c8c
fcapallera <fcapallera@gmail.com>
2020-01-31 09:41:23 +0100

    Added new controllers

---
+++ Controllers/NotifyController.cs
@@ -0,0 +1,74 @@
+﻿using CoreBot.Models;
+using CoreBot.Store;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoreBot.Controllers
+{
+    public class NotifyController
+    {
+        private readonly IBotFrameworkHttpAdapter _adapter;
+        private readonly string _appId;
+        private readonly ConcurrentDictionary<string, ConversationReference> _conversationReferences;
+        private readonly IPrestashopApi PrestashopApi;
+        private readonly UserController UserController;
+
+        public NotifyController(IBotFrameworkHttpAdapter adapter, ConcurrentDictionary<string,ConversationReference> references,
+            UserController userController, IConfiguration configuration, IPrestashopApi prestashopApi)
+        {
+            _adapter = adapter;
+            _conversationReferences = references;
+            UserController = userController;
+            _appId = configuration["MicrosoftAppId"];
+            PrestashopApi = prestashopApi;
+
+            if (string.IsNullOrEmpty(_appId))
+            {
+                _appId = Guid.NewGuid().ToString(); //if no AppId, use a random Guid
+            }
+        }
+
+        public async Task RequestValidationAsync(string id)
+        {
+            var superusers = await UserController.GetUsersByPermissionLevelAsync(0);
+
+            var requestingUser = await UserController.GetUserByBotIdAsync(id);
+
+            var prestashopUser = (await PrestashopApi.GetCustomerById(requestingUser.PrestashopId.Value)).First();
+
+            // We declare a local function to use as BotCallBackHandler in the ContinueConversationAsync Method
+            async Task botCallBack(ITurnContext turnContext, CancellationToken cancellationToken) =>
+                    await turnContext.SendActivityAsync($"User {prestashopUser.GetFullName()} would like to get his account validated.");
+
+            foreach (Models.UserProfile superuser in superusers)
+            {
+                var conversationReference = _conversationReferences[superuser.BotUserId];
+
+                await ((BotAdapter)_adapter).ContinueConversationAsync(_appId, conversationReference, botCallBack, default);
+            }
+        }
+
+
+        public async Task NotifyValidation(int prestashopId)
+        {
+            var user = await UserController.GetUserByPrestashopIdAsync(prestashopId);
+
+            var conversationReference = _conversationReferences[user.BotUserId];
+
+            async Task botCallBack(ITurnContext turnContext, CancellationToken cancellationToken) =>
+                    await turnContext.SendActivityAsync("Your account has been validated, you can start using Greta in its full potential!");
+
+            await ((BotAdapter)_adapter).ContinueConversationAsync(_appId, conversationReference, botCallBack, default);
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,95 @@
+﻿using CoreBot.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBot.Controllers
+{
+    public class UserController
+    {
+        private readonly IServiceProvider ServiceProvider;
+
+        public UserController(IServiceProvider serviceProvider)
+        {
+            ServiceProvider = serviceProvider;
+        }
+
+        //TO_DO TFG EXPLICAR FUNCIONS ASÍNCRONES
+
+        public async Task AddUserAsync(Models.UserProfile userProfile)
+        {
+            using (var context = ServiceProvider.CreateScope())
+            {
+                var db = context.ServiceProvider.GetRequiredService<GretaDBContext>();
+
+                db.Add(userProfile);
+                await db.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<Models.UserProfile>> GetUsersByPermissionLevelAsync(int permissionLevel)
+        {
+            using (var context = ServiceProvider.CreateScope())
+            {
+                var db = context.ServiceProvider.GetRequiredService<GretaDBContext>();
+
+                return await db.UserProfile.Where(u => u.Permission == permissionLevel && u.BotUserId != null).ToListAsync();
+            }
+        }
+
+
+        public async Task<bool> CheckForUserProfileAsync(int prestaId)
+        {
+            var user = await GetUserByPrestashopIdAsync(prestaId);
+            return await Task.FromResult(user != null);
+        }
+
+
+        public async Task<bool> CheckForUserProfileAsync(string botId)
+        {
+            var user = await GetUserByBotIdAsync(botId);
+            return await Task.FromResult(user != null);
+        }
+
+
+        public async Task<Models.UserProfile> GetUserByPrestashopIdAsync(int prestashopId)
+        {
+            using (var context = ServiceProvider.CreateScope())
+            {
+                var db = context.ServiceProvider.GetRequiredService<GretaDBContext>();
+
+                return await db.UserProfile.Where(u => u.PrestashopId == prestashopId).FirstOrDefaultAsync();
+            }
+        }
+
+        // If no user is found, a null object is returned instead
+        public async Task<Models.UserProfile> GetUserByBotIdAsync(string botId)
+        {
+            using (var context = ServiceProvider.CreateScope())
+            {
+                var db = context.ServiceProvider.GetRequiredService<GretaDBContext>();
+
+                return await db.UserProfile.Where(u => u.BotUserId == botId).FirstOrDefaultAsync();
+            }
+        }
+
+
+        public async Task ValidateUserAsync(int prestashopId, int permission)
+        {
+            using (var context = ServiceProvider.CreateScope())
+            {
+                var db = context.ServiceProvider.GetRequiredService<GretaDBContext>();
+
+                var user = await db.UserProfile.Where(u => u.PrestashopId == prestashopId).FirstOrDefaultAsync();
+
+                user.Validated = true;
+                user.Permission = permission;
+
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
